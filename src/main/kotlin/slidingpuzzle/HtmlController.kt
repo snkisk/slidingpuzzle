d008075068eb29eecb04e8d4926ffa33cee26ef3
Naoya Akimoto <akitkat.cook.dx@gmail.com>
2020-04-05 02:13:10 +0900

    queryパラメータないときは初期値をn=5に設定.

--- src/main/kotlin/slidingpuzzle/HtmlController.kt
+++ src/main/kotlin/slidingpuzzle/HtmlController.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam
 class GameController {
 
   @GetMapping("/")
-  fun game(@RequestParam n: Int, model: Model): String {
+  fun game(@RequestParam(value = "n", required = false, defaultValue = "5") n : Int, model: Model): String {
     model.addAttribute("cells", (1..n).toList())
     model.addAttribute("flexSize", 100/(1..n).toList().size)
     return "game"
