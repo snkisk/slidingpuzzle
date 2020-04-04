2fc7fd1c81df4fc32efae32a4ad3126915fdf4db
Naoya Akimoto <akitkat.cook.dx@gmail.com>
2020-04-05 01:41:31 +0900

    queryパラメータでマス数を増やせるように変更.

--- src/main/kotlin/slidingpuzzle/HtmlController.kt
+++ src/main/kotlin/slidingpuzzle/HtmlController.kt
@@ -4,12 +4,15 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 class GameController {
 
   @GetMapping("/")
-  fun game(model: Model): String {
+  fun game(@RequestParam n: Int, model: Model): String {
+    model.addAttribute("cells", (1..n).toList())
+    model.addAttribute("flexSize", 100/(1..n).toList().size)
     return "game"
   }
 }
