8cbc034014080b03019685a03336660152564d05
Naoya Akimoto <akitkat.cook.dx@gmail.com>
2020-04-05 14:56:47 +0900

    サーバサイドでゲーム盤面を定義して使用できるようにする

--- src/main/kotlin/slidingpuzzle/HtmlController.kt
+++ src/main/kotlin/slidingpuzzle/HtmlController.kt
@@ -11,7 +11,11 @@ class GameController {
 
   @GetMapping("/")
   fun game(@RequestParam(value = "n", required = false, defaultValue = "5") n : Int, model: Model): String {
-    model.addAttribute("cells", (1..n).toList())
+    val cells: Array<Array<String>> = Array(n, {Array(n, {""})})
+    cells[0][n-1] = "goal"
+    cells[n-2][0] = "block"
+    cells[n-1][0] = "start"
+    model.addAttribute("cells", cells)
     model.addAttribute("flexSize", 100/(1..n).toList().size)
     return "game"
   }
