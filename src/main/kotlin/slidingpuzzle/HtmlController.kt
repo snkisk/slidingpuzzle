7861ed87c0c591baa136be1ab00b271f3ab6bf57
Naoya Akimoto <akitkat.cook.dx@gmail.com>
2020-04-10 01:22:43 +0900

    DBから盤面を取得できるように変更.

--- src/main/kotlin/slidingpuzzle/HtmlController.kt
+++ src/main/kotlin/slidingpuzzle/HtmlController.kt
@@ -7,16 +7,18 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
-class GameController {
+class GameController(private val stageMasterRepository: StageMasterRepository, private val stageCellsRepository: StageCellsRepository) {
 
   @GetMapping("/")
   fun game(@RequestParam(value = "n", required = false, defaultValue = "5") n : Int, model: Model): String {
-    val cells: Array<Array<String>> = Array(n, {Array(n, {""})})
-    cells[0][n-1] = "goal"
-    cells[n-2][0] = "block"
-    cells[n-1][0] = "start"
-    model.addAttribute("cells", cells)
     model.addAttribute("flexSize", 100/(1..n).toList().size)
+
+    val master = stageMasterRepository.findOneByCellsCount(n)
+    if (master == null)
+      return "redirect:/"
+
+    val (id, cellsCount) = master
+    model.addAttribute("cells", stageCellsRepository.getStage(id).windowed(size = n, step = n))
     return "game"
   }
 }
